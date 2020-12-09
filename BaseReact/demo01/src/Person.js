08617b0a90ca1b4dfa9332e2fc23487f5984a3f4
毛毛虫小姐 <13007107675@163.com>
2020-12-09 21:55:41 +0800

    fix: 生命周期 axios

--- BaseReact/demo01/src/Person.js
+++ BaseReact/demo01/src/Person.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import PersonItem from './PersonItem'
-
+import axios from 'axios'
 
 import './style.css'
 class Person extends Component {
@@ -10,37 +10,66 @@ class Person extends Component {
       inputValue: '',
       list: ['基础按摩','精油推背']
     }
-
     this.addServe = this.addServe.bind(this)
     this.deleteServe = this.deleteServe.bind(this)
   }
+  // TODO 生命周期
+  // ? 组件挂载之前
+  // componentWillMount() {
+  // }
+  // ? 组件挂载结束
+  componentDidMount() {
+    console.log(axios)
+    // ! 一般在dom 挂载结束之后 加载异步数据
+    // axios.post(url, ) 
+    axios.post('https://baidu.com')
+      .then(res => { console.log(`获取成功${res}`) })
+      .catch((error) => { console.log(`获取失败${error}`)})
+  }
+  // ? 是否可以更新 需要返回一个 boolean 
+  //   todo 返回 true:更新之后继续执行 false: 更新停止 
+  // shouldComponentUpdate() {
+  //   return true
+  // }
+  // ?开始更新
+  // componentWillUpdate() {
+  // }
+  // ? 更新结束
+  // componentDidUpdate() {
+  // }
   render() {
+    // console.log('render ---------> 组件挂载中')
     return (
       <Fragment>
         <div>
           <label htmlFor="add">增加服务：</label>
-          <input id="add" className="input-box" value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
+          <input
+            id="add"
+            className="input-box"
+            value={this.state.inputValue}
+            onChange={this.inputChange.bind(this)}
+            ref= {(input)=>{this.input = input}}
+          />
           <button className="add-btn" onClick={this.addServe}>增加服务</button>
         </div>
-        <ul>
+        <ul ref={(ul)=>{this.ul = ul}}>
           {
-            <PersonItem
-             list={this.state.list}
-             deleteServe={this.deleteServe}
-           />
-            // return (
-            // this.state.list.map((e, index) => {
-            //   return (
-            //     <PersonItem
-            //       key={index}
-            //       content={e}
-            //       index={index}
-            //       list={this.state.list}
-            //       deleteServe={this.deleteServe}
-            //     />
-            //   )
-            // })
-            // )
+          //   <PersonItem
+          //    list={this.state.list}
+          //    deleteServe={this.deleteServe}
+          //  />
+            this.state.list.map((e, index) => {
+              return (
+                <PersonItem
+                  key={index}
+                  content={e}
+                  index={index}
+                  list={this.state.list}
+                  deleteServe={this.deleteServe}
+                  name="Jack"
+                />
+              )
+            })
           } 
         </ul>
       </Fragment>
@@ -49,7 +78,7 @@ class Person extends Component {
   inputChange(e) {
     // ! 修改数据 需要通过setState 来修改
     this.setState({
-      inputValue: e.target.value
+      inputValue: this.input.value
     })
   }
   addServe() {
@@ -58,7 +87,12 @@ class Person extends Component {
     this.setState({
       list: [...this.state.list, this.state.inputValue],
       inputValue: ''
-    })
+    }, () => {
+      // TODO dom 更新是异步的因此需要通过回调来获取最新值
+        console.log(this.state.list.length)
+        // ? 通过 ref 来获取 ul 下 li 的个数
+      console.log(this.ul.querySelectorAll('li').length)
+    })    
   }
   deleteServe(index) {
     // ! 不可以直接操作 this 里面的数据 
--- BaseReact/demo01/src/PersonItem.js
+++ BaseReact/demo01/src/PersonItem.js
@@ -1,32 +1,56 @@
 import React, { Component } from 'react';
-class PersonItem extends Component {
+import PropTypes from 'prop-types'
 
+
+class PersonItem extends Component {
   constructor(props) {
     super(props)
     this.deleteItem = this.deleteItem.bind(this)
   }
-  // state = {  }
+  // todo 当前函数第一次存于 DOM 中， 函数不会执行
+  // todo 已经存在于 DOM 中，函数才会被执行
+  // ! 渲染子组件第一改不会执行，发生变化 第二次执行时才会执行
+  // componentWillReceiveProps() {
+  //   console.log('child ----> componentWillReceiveProps')
+  // }
+
+  // 组件性能优化
+  // ! 避免出现经常性 render 子组件
+  shouldComponentUpdate(nextProp, nextState) {
+    if (nextProp.content !== this.props.content) {
+      return true
+    } else {
+      return false
+    }
+  }
 
   render() { 
     return ( 
-      <div>
-        {
-          this.props.list.map((e, index) => {
-            return (
-              <li key={index} onClick={this.deleteItem.bind(this, index)}>{e}</li>
-            )
-          })
-        }
-      </div>
-      // <li onClick={this.deleteItem}>{ this.props.content }</li>
+      <li onClick={this.deleteItem}>
+        {this.props.age}岁{this.props.name}:为你-{ this.props.content}
+      </li>
     );
   }
   deleteItem(index) {
     // ! 子组件不可直接修改父组件中的数据 
     // TODO 需要通过调用父组件中的方法来操作数据从而更新DOM
-    console.log(index)
-    this.props.deleteServe(index)
+    // this.props.deleteServe(index)
+    this.props.deleteServe(this.props.index)
   }
 }
+
+// prop 校验
+PersonItem.propTypes = {
+  // isRequired 校验前必须传递 且为 string 类型
+  name: PropTypes.string,
+  content: PropTypes.string.isRequired,
+  index: PropTypes.number,
+  deleteItem: PropTypes.func, 
+}
+// props 可不传给出默认值，通过defaultProps 的方式
+PersonItem.defaultProps = {
+  age: '20'
+}
+
  
 export default PersonItem;
