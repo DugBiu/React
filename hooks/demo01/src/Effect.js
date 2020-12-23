c7d2419fd9cd2edea7e25310d5acdf2af56ffd54
毛毛虫小姐 <13007107675@163.com>
2020-12-23 22:32:54 +0800

    fix： 方法使用

--- hooks/demo01/src/Effect.js
+++ hooks/demo01/src/Effect.js
@@ -29,53 +29,7 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
   //   console.log('componentDidUpdate: '+ this.state.count)
   // }
 // }
-// // ? 组件一
-// function Index() {
-//   useEffect(() => {
-//     console.log('index 进入首页')
-//     // ! 解绑 相当于 componentwillunmount
-//     // TODO解绑时可以做的操作
-//     // TODO 状态发生变化时， 都会进入解绑函数
-//     // TODO 解绑副作用：useEffect() 添加第二个参数
-//     return () => {
-//       console.log('index 离开首页')
-//     }
-//     // ? [count]: 只有count 发生变化时，进入这里
-//     // ? []: 只有当前组件被解绑时 才执行
-//   },[])
-//   return(<h2>首页</h2>)
-// }
-// // ? 组件一
-// function List() {
-//   useEffect(() => {
-//     console.log('进入列表')
-//   })
-//   return(<h2>列表页</h2>)
-// }
-// const Effect = () => {
-//   const [count, setCount] = useState(0)
-//   // TODO useEffect => componentDidMount、componentDidUpdate
-//   useEffect(() => {
-//     console.log('===================== '+count)
-//   },[count])
-//   return (
-//     <div className="box">
-//       <p>useEffect 可代替 componentDidMount、componentDidUpdate、componentwillunmount</p>
-//       <h2>{count}</h2>
-//       <button onClick={() => { setCount(count + 1) }}> Click Me</button>
-      
-//       <Router>
-//         <ul>
-//           <li><Link to="/index">首页</Link></li>
-//           <li><Link to="/list">列表</Link></li>
-//         </ul>
-//         <Route path="/index" exact component={Index}></Route>
-//         <Route path="/list"  component={List}></Route>
-//       </Router>
-//     </div>
-//   )
-// }
- 
+//  ! useState useEffect 的方式
 const Index = () => {
   useEffect(() => {
     console.log('index ==========> 进入')
